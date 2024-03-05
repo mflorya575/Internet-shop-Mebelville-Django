74f9e8e5328c336ee2e012b30aff69f903d2b5d1
Michael Florya <mih2303@mail.ru>
2024-03-05 19:13:55 +0300

    register simple tag for categories

--- shop/main/views.py
+++ shop/main/views.py
@@ -6,12 +6,9 @@ from goods.models import Categories
 
 def index(request):
 
-    categories = Categories.objects.all()
-
     context = {
         'title': 'Главная - Mebelville',
         'content': 'Магазин мебели Mebelville',
-        'categories': categories,
     }
 
     return render(request, 'main/index.html', context)
