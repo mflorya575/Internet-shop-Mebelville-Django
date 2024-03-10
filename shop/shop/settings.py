8b4df5a2032f42ffb31b68586788c7458fd87c4b
Michael Florya <mih2303@mail.ru>
2024-03-10 17:09:01 +0300

    install psycopg2, improve README, create new fixtures, new DB PostgreSQL

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'home',
+        'PASSWORD': '54321',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
